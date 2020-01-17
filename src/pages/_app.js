a32487c7e31875e111ec67e0cdd2f3c82a5bc195
Luong Quang Manh <kysumot@teneocto.io>
2020-01-17 17:49:45 +0700

    Add Redux store

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,11 @@
-import App from 'next/app'
+import { Provider } from 'react-redux'
+import store from '../redux/store'
 import '../styles/index.css'
+
+const App = ({ Component, pageProps }) => (
+  <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
+)
 
 export default App
---
+++ src/redux/reducers.js
@@ -0,0 +1,3 @@
+const rootReducer = null
+
+export { rootReducer }
